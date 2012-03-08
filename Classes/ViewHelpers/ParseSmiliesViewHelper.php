f6ad17213eac003530d0d55d2394dd99746c466b
alexw <aweisswa@gmx.de>
2012-03-08 01:42:44 +0100

    [FEATURE] Add smilie support in wall, messages and profile
    Change-Id: I8deebefa7009966cb5b14bf19ba23b6b9e2ead2d
    Related: #34657

---
+++ Classes/ViewHelpers/ParseSmiliesViewHelper.php
@@ -0,0 +1,53 @@
+<?php
+/***************************************************************
+ *  Copyright notice
+ *
+ *  (c) 2012 Alexander Weiß
+ *
+ *  All rights reserved
+ *
+ *  This script is part of the TYPO3 project. The TYPO3 project is
+ *  free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  The GNU General Public License can be found at
+ *  http://www.gnu.org/copyleft/gpl.html.
+ *
+ *  This script is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  This copyright notice MUST APPEAR in all copies of the script!
+ ***************************************************************/
+
+/**
+ * Parses a string for Smilies and replaces them with small images
+ *
+ * @copyright Copyright belongs to the respective authors
+ * @license http://www.gnu.org/licenses/gpl.html GNU General Public License, version 3 or later
+ */
+class Tx_Community_ViewHelpers_ParseSmiliesViewHelper extends Tx_Fluid_Core_ViewHelper_AbstractViewHelper {
+
+	/**
+	* Renders the SmiliesViewHelper
+	*
+	* @param string $string String with smilie codes
+	*
+	* @return string String with smilie images
+	*/
+	public function render($string = '') {
+		if ($string === '') {
+			$string = $this->renderChildren();
+		}
+		// if ext:smilie is installed we will automatically replace the smilies
+		if (t3lib_extMgm::isLoaded('smilie')) {
+			$smilie = t3lib_div::makeInstance('tx_smilie'); /* @var $smilie tx_smilie */
+			return $smilie->replaceSmilies($string);
+		}
+		return $string;
+	}
+}
+?>
--- ext_emconf.php
+++ ext_emconf.php
@@ -40,6 +40,7 @@ $EM_CONF[$_EXTKEY] = array(
 		'conflicts' => array(
 		),
 		'suggests' => array(
+			'smilie' => ''
 		),
 	),
 );
